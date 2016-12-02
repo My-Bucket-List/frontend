e287e46287d8ab5835f367acfe292e9ab4d02911
Alex Windle <alexwindle85@gmail.com>
2016-12-02 12:11:23 -0500

    WIP - Incremental changes to controllers. Connection to backend not yet established.

--- src/js/controllers/GoalsController.js
+++ src/js/controllers/GoalsController.js
@@ -1,6 +1,17 @@
-function GoalsController($rootScope){
+const SERVER = 'https://sleepy-island-23804.herokuapp.com/'; 
+
+function GoalsController($http){
 	let vm = this;
+
+	// this.addGoal = addGoal;
+
+	// function addGoal(banana){
+	// 	console.log(banana);
+	// 	$http.post(SERVER).then((resp)=>{
+	// 		console.log(resp);
+	// 	})
+	// }
 }
 
-GoalsController.$inject=['$rootScope'];
+GoalsController.$inject=['$http'];
 export { GoalsController };
--- src/js/controllers/HomeController.js
+++ src/js/controllers/HomeController.js
@@ -1,6 +1,15 @@
-function HomeController($rootScope){
-	let vm = this;
+const SERVER = 'https://sleepy-island-23804.herokuapp.com/';
+
+function HomeController(){
+	
+	// let vm = this;
+
+	// function init(){
+	// 	http.get(SERVER).then((resp)=>{
+	// 		console.log(resp);
+	// 	})
+	// }
 }
 
-HomeController.$inject=['$rootScope'];
+HomeController.$inject=[];
 export { HomeController }; 
