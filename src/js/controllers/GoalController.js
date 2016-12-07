60fa533b5ca8359c3e821ad906186dbdee49546b
Alex Windle <alexwindle85@gmail.com>
2016-12-07 16:11:58 -0500

    Clean up code.

--- src/js/controllers/GoalController.js
+++ src/js/controllers/GoalController.js
@@ -4,21 +4,17 @@ function GoalController($cookies, $http, $state, GoalService, UserService, $stat
 
 	vm.goals = []; 
 	vm.add = add;
-	vm.showGoals = false;
 	vm.userId = $cookies.get('id'); 
 
 	function add (goal) {
 			GoalService.addGoal(goal).then((resp)=>{
-			console.log('New goal: ', resp.data); 	
 			vm.goals.push(resp.data); 
 			$state.go('root.goals'); 
 		}); 
 	}; 
 
 	function init () {
-			console.log('CTRL id: ', vm.userId); 
 			GoalService.getUserGoals(vm.userId).then((resp)=>{
-				console.log('response: ', resp.data); 
 				vm.goals = resp.data; 
 		}); 
 	}; 
