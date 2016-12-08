6fbfe5c529c8fd600f3290b5ad474b3db6d87085
Alex Windle <alexwindle85@gmail.com>
2016-12-08 16:16:16 -0500

    Filter goals list based on completed:true.

--- src/js/controllers/AccomplishController.js
+++ src/js/controllers/AccomplishController.js
@@ -1,6 +1,28 @@
-function AccomplishController($rootScope){
+function AccomplishController($rootScope, GoalService, $cookies){
+	
 	let vm = this;
+	vm.userId = $cookies.get('id'); 
+	vm.goals = []; 
+
+	function checkCompleteStatus (item) {
+		return item.completed == true;
+	}; 
+
+	function init () {
+		GoalService.getUserGoals(vm.userId).then((resp)=>{
+			console.log('resp: ', resp); 
+			
+			let goalList = resp.data.filter(checkCompleteStatus); 
+
+			vm.goals = goalList; 
+
+			// vm.goals = resp.data; 
+
+		}); 
+	}; 
+
+	init(); 
 }
 
-AccomplishController.$inject=['$rootScope'];
+AccomplishController.$inject=['$rootScope', 'GoalService', '$cookies'];
 export { AccomplishController };
